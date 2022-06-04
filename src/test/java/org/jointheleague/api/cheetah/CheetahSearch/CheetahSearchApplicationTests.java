494ce640a7036d15570435004ec3286a0f58b373
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2022-06-04 13:58:13 -0700

    initial

---
+++ src/test/java/org/jointheleague/api/cheetah/CheetahSearch/CheetahSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.cheetah.CheetahSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheetahSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
